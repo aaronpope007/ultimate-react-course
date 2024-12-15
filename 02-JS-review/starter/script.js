b1f246a00941074bef3e9c7e76bf09de115abb00
Aaron Pope <aapope@centralstatesfunds.org>
2024-12-14 21:03:53 -0600

    async await

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -319,10 +319,17 @@ booksAfterUpdate;
 // Async JS promises
 // fetch api
 
-fetch("https://jsonplaceholder.typicode.com/todos/")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos/")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-console.log("jonas");
+// console.log("jonas");
 
-// asynch await
+// async await
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data = await res.json();
+  console.log(data);
+}
+getTodos();
